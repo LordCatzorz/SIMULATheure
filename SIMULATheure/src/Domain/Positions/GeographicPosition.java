da9d1d976c937545f50b44ff18c375371aa7cb96
PATP4SE <p-a_morin@hotmail.com>
2015-11-10 00:53:12 -0500

    geographicPosition

--- SIMULATheure/src/Domain/Positions/GeographicPosition.java
+++ SIMULATheure/src/Domain/Positions/GeographicPosition.java
@@ -11,5 +11,38 @@ package Domain.Positions;
  */
 public class GeographicPosition implements java.io.Serializable
 {
+    private GeographicCoordinate longitude;
+    private GeographicCoordinate latitude;
     
+    public GeographicPosition(GeographicCoordinate _longitude, GeographicCoordinate _latitude)
+    {
+        this.longitude = _longitude;
+        this.latitude = _latitude;
+    }
+    
+    public GeographicCoordinate getLongitude()
+    {
+        return this.longitude;
+    }
+    
+    public void setLongitude(GeographicCoordinate _longitude)
+    {
+        this.longitude = _longitude;
+    }
+    
+    public GeographicCoordinate getLatitude()
+    {
+        return this.latitude;
+    }
+    
+    public void setLatitude(GeographicCoordinate _latitude)
+    {
+        this.latitude = _latitude;
+    }
+    
+    public float getDistance(GeographicPosition _geographicPosition)
+    {
+        return 0;
+    }
 }
+
